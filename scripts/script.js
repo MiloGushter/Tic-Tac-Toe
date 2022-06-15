3c2ae4c563c04fe7a31b54e9d9a4a654bb358e50
Luka <milojevic.luka2002@gmail.com>
2022-06-15 23:33:06 +0200

    Added player input

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,75 @@ const gameBoard = (() => {
   return { board };
 })();
 
+const playerCreator = function (name, role) {
+  return { name, role };
+};
+
 const displayController = (() => {
+  // Information input controll
+  let firstPlayerRadioX = document.querySelector("#player1x");
+  let firstPlayerRadioO = document.querySelector("#player1o");
+  let secondPlayerRadioX = document.querySelector("#player2x");
+  let secondPlayerRadioO = document.querySelector("#player2o");
+
+  firstPlayerRadioX.addEventListener("click", () => {
+    secondPlayerRadioO.checked = true;
+  });
+
+  secondPlayerRadioO.addEventListener("click", () => {
+    firstPlayerRadioX.checked = true;
+  });
+
+  firstPlayerRadioO.addEventListener("click", () => {
+    secondPlayerRadioX.checked = true;
+  });
+
+  secondPlayerRadioX.addEventListener("click", () => {
+    firstPlayerRadioO.checked = true;
+  });
+
+  const btn1 = document.querySelector(".submit-info1");
+  btn1.addEventListener("click", () => {
+    const input = document.querySelector("#player1");
+    let playerName = input.value;
+    if (playerName === "") {
+      alert("Enter the name!");
+      return;
+    }
+    if (
+      firstPlayerRadioO.checked !== true &&
+      firstPlayerRadioX.checked !== true
+    ) {
+      alert("You must choose the role!");
+    }
+    if (firstPlayerRadioX.checked)
+      firstPlayer = playerCreator(playerName, firstPlayerRadioX.value);
+    else if (firstPlayerRadioO.checked)
+      firstPlayer = playerCreator(playerName, firstPlayerRadioO.value);
+    input.value = "";
+  });
+
+  const btn2 = document.querySelector(".submit-info2");
+  btn2.addEventListener("click", () => {
+    const input = document.querySelector("#player2");
+    let playerName = input.value;
+    if (playerName === "") {
+      alert("Enter the name!");
+      return;
+    }
+    if (
+      secondPlayerRadioO.checked !== true &&
+      secondPlayerRadioX.checked !== true
+    ) {
+      alert("You must choose the role!");
+    }
+    if (secondPlayerRadioX.checked)
+      secondPlayer = playerCreator(playerName, secondPlayerRadioX.value);
+    else if (secondPlayerRadioO.checked)
+      secondPlayer = playerCreator(playerName, secondPlayerRadioO.value);
+    input.value = "";
+  });
+
   //X is always first to be played
   let playerX = true;
   let playerO = false;
@@ -12,8 +80,18 @@ const displayController = (() => {
   fields.forEach((field) => {
     field.addEventListener("click", () => {
       let fieldIndex = Array.from(fields).indexOf(field);
-      if (field.textContent !== "") {
+      if (
+        (field.textContent === "x" && playerO === true) ||
+        (field.textContent === "o" && playerX === true)
+      ) {
         alert("That's is illegal!");
+      }
+      // TODO: This should be turned into erase of the field
+      else if (
+        (field.textContent === "x" && playerX === true) ||
+        (field.textContent === "o" && playerO === true)
+      ) {
+        alert("You have already played here!");
       }
       if (playerX === true && field.textContent === "") {
         field.textContent = "x";
@@ -29,3 +107,6 @@ const displayController = (() => {
     });
   });
 })();
+
+let firstPlayer;
+let secondPlayer;
